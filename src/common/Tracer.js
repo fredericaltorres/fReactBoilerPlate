2eb3e3594c2805a33fae56089f8dc645161e26cd
Frederic Torres <fredericaltorres@gmail.com>
2019-04-13 22:35:51 -0400

    Add possibility to turn off colored console message
    add property coloredConsole to turn of the color console mode not supported by IE11 or NativeScript

--- src/common/Tracer.js
+++ src/common/Tracer.js
@@ -3,10 +3,16 @@ class Tracer {
 
 	constructor() {
 		this.traceOn = true;
+		this.coloredConsole = true;
 	}
 	internalTrace(text, style0, style1, logMethod) {
-		if (this.traceOn) 
-			logMethod(text, style0, style1);
+		if (this.traceOn) {
+			if(this.coloredConsole)
+				logMethod(text, style0, style1);
+			else 
+				logMethod(text);
+		}
+			
 	}
 	getName(instance) {
 		if (instance && instance.name) return instance.name;
@@ -35,12 +41,22 @@ class Tracer {
 	}
 	log(m, instance = null) {
 		const mm = this.getPrefix(instance);
-		this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:blue;', console.log);
+		if(this.coloredConsole) {
+			this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:blue;', console.log);
+		}
+		else {
+			this.internalTrace(`${mm} ${m}`, 'color:green;', 'color:blue;', console.log);
+		}
 		return mm + m;
 	}
 	warn(m, instance = null) {
 		const mm = `[${this.getTimeStamp()}, ${this.getName(instance)}]`;
-		this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:blue;', console.warn);
+		if(this.coloredConsole) {
+			this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:blue;', console.warn);
+		}
+		else {
+			this.internalTrace(`${mm} ${m}`, 'color:green;', 'color:blue;', console.warn);
+		}
 		return mm + m;
 	}
 	notifyUser(m, instance = null) {
@@ -49,7 +65,12 @@ class Tracer {
 	}
 	error(m, instance = null) {
 		const mm = `[${this.getTimeStamp()}, ${this.getName(instance)}]`;
-		this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:red;', console.error);
+		if(this.coloredConsole) {
+			this.internalTrace(`%c ${mm}%c ${m}`, 'color:green;', 'color:red;', console.error);
+		}
+		else {
+			this.internalTrace(`${mm} ${m}`, 'color:green;', 'color:red;', console.error);
+		}
 		return mm + m;
 	}
 	throw (error, instance = null) {
